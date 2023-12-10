1461b7438a03afbcd9def1bd1fbf745e14d29722
Prudencerich <maryejiropraise@gmail.com>
2023-12-10 18:08:00 +0100

    online shopping website

--- myfirstproject/item/models.py
+++ myfirstproject/item/models.py
@@ -12,10 +12,11 @@ class Category(models.Model):
         return self.name
     
 class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='item', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
-    image = models.ImageField(upload_to='Item_images', blank=True, null=True)
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
